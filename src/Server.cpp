d883d874136edea4c9641ace18f7244ba2bfb39d
Robert McMahon <rjmcmahon@rjmcahon.com>
2019-10-03 16:32:04 -0700

    apply De Morgan's law properly on UDP no fin test w/server

--- src/Server.cpp
+++ src/Server.cpp
@@ -594,7 +594,7 @@ void Server::RunUDP( void ) {
     // send a acknowledgement back except when:
     // 1) we're NOT receiving multicast
     // 2) the user requested no final exchange
-    if (!isMulticast(mSettings) || !isNoUDPfin(mSettings)) {
+    if (!isMulticast(mSettings) && !isNoUDPfin(mSettings)) {
 	// send back an acknowledgement of the terminating datagram
 	write_UDP_AckFIN( );
     }
