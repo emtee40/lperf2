8c22a56e8eb1a9bda37ea4b32858bb36030d8589
Robert McMahon <rjmcmahon@rjmcahon.com>
2019-10-31 12:43:12 -0700

    bump date

--- src/Locale.c
+++ src/Locale.c
@@ -284,7 +284,7 @@ const char report_bw_read_enhanced_netpwr_header[] =
 "[ ID] Interval" IPERFTimeSpace "Transfer    Bandwidth       Reads   Dist(bin=%.1fK)     Latency avg/min/max/stdev inP NetPwr\n";
 
 const char report_bw_read_enhanced_netpwr_format[] =
-"[%3d] " IPERFTimeFrmt " sec  %ss  %ss/sec  %d    %d:%d:%d:%d:%d:%d:%d:%d %6.3f/%6.3f/%6.3f/%6.3f ms %2.0f bytes %4.2f\n";
+"[%3d] " IPERFTimeFrmt " sec  %ss  %ss/sec  %d    %d:%d:%d:%d:%d:%d:%d:%d    %6.3f/%6.3f/%6.3f/%6.3f ms %2.0f bytes %4.2f\n";
 
 const char report_sum_bw_read_enhanced_format[] =
 "[SUM] " IPERFTimeFrmt " sec  %ss  %ss/sec  %d    %d:%d:%d:%d:%d:%d:%d:%d\n";
