652ccdebb9c0c9bc607c890d0a8fcca9460064be
Robert McMahon <rjmcmahon@rjmcmahon.com>
2020-09-07 20:33:04 -0700

    fixes to bidir barrier

--- src/Launch.cpp
+++ src/Launch.cpp
@@ -75,7 +75,7 @@ static int bidir_startstop_barrier (struct BarrierMutex *barrier) {
 #ifdef HAVE_THREAD_DEBUG
 	thread_debug("BiDir startstop broadcast on condition %p ", (void *)&barrier->await, rc);
 #endif
-	barrier_count--;
+	barrier->count--;
 	Condition_Broadcast(&barrier->await);
     } else {
 	int timeout = barrier->timeout;
@@ -89,7 +89,7 @@ static int bidir_startstop_barrier (struct BarrierMutex *barrier) {
 		fprintf(stdout, "Barrier timeout per full duplex traffic\n");
 	    }
 	}
-	barrier_count--;
+	barrier->count--;
     }
     Condition_Unlock(barrier->await);
     return rc;
