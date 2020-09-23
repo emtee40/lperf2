b92f327fe4df6a1b21af6fb0a899432ec4ea3b05
Robert McMahon <rjmcmahon@rjmcmahon.com>
2020-09-23 12:59:05 -0700

    send first payload on reverse or bidir and isoch or trip times

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1366,7 +1366,8 @@ void Settings_ReadClientSettingsIsoch (struct thread_Settings **client, struct c
 }
 
 void Settings_ReadClientSettingsV1 (struct thread_Settings **client, struct client_hdr_v1 *hdr) {
-    setCompat((*client)); // Disable test headers for this client
+    if (!isTripTime((*client)) || !isIsoch((*client));
+	setCompat((*client)); // Disable test headers for this reversed client
     (*client)->mTID = thread_zeroid();
     (*client)->mPort = (unsigned short) ntohl(hdr->mPort);
     (*client)->mThreads = 1;
