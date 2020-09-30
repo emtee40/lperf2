b8d51875b8225f9b772c40b1b20df51151d1f741
rmcmahon <bob.mcmahon@broadcom.com>
2020-09-30 15:19:19 -0700

    configure test for IP_MULTICAST_ALL

--- src/Listener.cpp
+++ src/Listener.cpp
@@ -431,9 +431,11 @@ void Listener::my_listen (void) {
 	if (SockAddr_isMulticast(&mSettings->local)) {
 #ifdef HAVE_MULTICAST
 	    my_multicast_join();
+#ifdef HAVE_DECL_IP_MULTICAST_ALL
 	    int mc_all = 0;
 	    rc = setsockopt(ListenSocket, IPPROTO_IP, IP_MULTICAST_ALL, (void*) &mc_all, sizeof(mc_all));
 	    WARN_errno(rc == SOCKET_ERROR, "ip_multicast_all");
+#endif
 #else
 	    fprintf(stderr, "Multicast not supported");
 #endif // HAVE_MULTICAST
