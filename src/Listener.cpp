f061f6fbe8071aa6c84c4d86e8bc5fc7d7a85868
Robert McMahon <rjmcmahon@rjmcmahon.com>
2020-10-01 00:17:25 -0700

    fix for Mac OS X

--- src/Listener.cpp
+++ src/Listener.cpp
@@ -431,7 +431,7 @@ void Listener::my_listen (void) {
 	if (SockAddr_isMulticast(&mSettings->local)) {
 #ifdef HAVE_MULTICAST
 	    my_multicast_join();
-#ifdef HAVE_DECL_IP_MULTICAST_ALL
+#if HAVE_DECL_IP_MULTICAST_ALL
 	    int mc_all = 0;
 	    rc = setsockopt(ListenSocket, IPPROTO_IP, IP_MULTICAST_ALL, (void*) &mc_all, sizeof(mc_all));
 	    WARN_errno(rc == SOCKET_ERROR, "ip_multicast_all");
