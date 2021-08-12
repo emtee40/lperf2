f0ec47f00db4589ada3742827c83b8412bef37ce
Robert McMahon <rjmcmahon@rjmcmahon.com>
2021-08-12 14:06:16 -0700

    master merge

--- include/version.h
+++ include/version.h
@@ -1,4 +1,25 @@
-#define IPERF_VERSION "2.1.n-master"
+/*
+ * 	case 0:
+ *	    sprintf(report->peerversion + strlen(report->peerversion) - 1,"-dev)");
+ *	    break;
+ *	case 1:
+ *	    sprintf(report->peerversion + strlen(report->peerversion) - 1,"-rc)");
+ *	    break;
+ *	case 2:
+ *	    sprintf(report->peerversion + strlen(report->peerversion) - 1,"-rc2)");
+ *	    break;
+ *	case 3:
+ *	    break;
+ *	case 4:
+ *	    sprintf(report->peerversion + strlen(report->peerversion) - 1,"-private)");
+ *	    break;
+ *	case 5:
+ *	    sprintf(report->peerversion + strlen(report->peerversion) - 1,"-master)");
+ * 	    break;
+ *	default:
+ *	    sprintf(report->peerversion + strlen(report->peerversion) - 1, "-unk)");
+ */
+#define IPERF_VERSION "2.1.4-rc"
 #define IPERF_VERSION_DATE "12 August 2021"
 #define IPERF_VERSION_MAJORHEX 0x00020001
-#define IPERF_VERSION_MINORHEX 0x00040005
+#define IPERF_VERSION_MINORHEX 0x00040001
