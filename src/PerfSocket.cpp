711ea80e289715df80254b2bd08ad047352f933b
Robert McMahon <rjmcmahon@rjmcmahon.com>
2021-11-23 21:14:50 -0800

    move setsock_blocking from sockets into PerfSocket

--- src/PerfSocket.cpp
+++ src/PerfSocket.cpp
@@ -347,4 +347,23 @@ void SetSocketOptionsIPTos (struct thread_Settings *mSettings, int tos) {
 }
 
 
+/*
+ * Set a socket to blocking or non-blocking
+*
+ * Returns true on success, or false if there was an error
+*/
+bool setsock_blocking (int fd, bool blocking) {
+   if (fd < 0) return false;
+#ifdef WIN32
+   unsigned long mode = blocking ? 0 : 1;
+   return (ioctlsocket(fd, FIONBIO, &mode) == 0) ? true : false;
+#else
+   int flags = fcntl(fd, F_GETFL, 0);
+   if (flags < 0) return false;
+   flags = blocking ? (flags&~O_NONBLOCK) : (flags|O_NONBLOCK);
+   return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
+#endif
+}
+
+
 // end SetSocketOptions
