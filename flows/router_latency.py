da6ac5a4522c58e94ab37ef6171787da893c69dc
rmcmahon <bob.mcmahon@broadcom.com>
2022-07-14 14:32:04 -0700

    minimal code for router_latency per opensource release

--- flows/router_latency.py
+++ flows/router_latency.py
@@ -47,13 +47,11 @@ import os,sys
 import ssh_nodes
 import gc
 import yaml
-import netlink
 
 from flows import *
 from ssh_nodes import *
 from config_yaml import *
 from ptp_test import *
-from netlink import *
 
 parser = argparse.ArgumentParser(description='Run a bufferbloat test')
 parser.add_argument('--config_file', type=str, required=True, help='Optional YAML config file to use')
@@ -65,30 +63,12 @@ parser.add_argument('-t','--time', type=float, default=30, required=False, help=
 parser.add_argument('-o','--output_directory', type=str, required=False, default='./pyflow_log', help='output directory')
 parser.add_argument('--test_name', type=str, default='lat1', required=False)
 parser.add_argument('--loglevel', type=str, required=False, default='INFO', help='python logging level, e.g. INFO or DEBUG')
-parser.add_argument('--apdump', dest='apdump', action='store_true', help='Enable AP dump', required=False)
-parser.add_argument('--apdump_period', type=float, default=5, required=False, help='time interval to run apdump')
-parser.add_argument('--ampdudump', dest='ampdudump', action='store_true', help='Enable ampdu dump', required=False)
-parser.add_argument('--netlink', dest='netlink', action='store_true', help='Enable netlink telemetry', required=False)
-parser.add_argument('--noptp', dest='noptp', action='store_true', help='PTP should not run', required=False)
-parser.add_argument('--ptp_recover', dest='ptp_recover', action='store_true', help='Recover PTP after failure', required=False)
+parser.add_argument('--ptp', dest='have_ptp', action='store_true', help='PTP is assumed', required=False)
 
-parser.set_defaults(netlink=False)
-parser.set_defaults(noptp=False)
-parser.set_defaults(ptp_recover=False)
-parser.set_defaults(apdump=False)
-parser.set_defaults(ampdudump=False)
+parser.set_defaults(have_ptp=False)
 
 # Parse command line arguments
 args = parser.parse_args()
-
-if args.noptp:
-    ptp_flag = False
-else:
-    ptp_flag = True
-    if args.ptp_recover:
-        ptp_recover_flag = True
-    else:
-        ptp_recover_flag = False
 
 # Set up logging below
 logfilename='main.log'
@@ -125,8 +105,8 @@ if str(yaml).find("TRAFFIC_DST2") != -1:
 if str(yaml).find("TRAFFIC_DST3") != -1:
     sta3_ip = yaml['TRAFFIC_DST3']['control_ip']
 
-if ptp_flag:
-    test_ptp_clock(ptp_recover_flag, [yaml['TRAFFIC_SRC']['control_ip'], sta1_ip, sta2_ip, sta3_ip])
+if args.have_ptp:
+    test_ptp_clock(False, [yaml['TRAFFIC_SRC']['control_ip'], sta1_ip, sta2_ip, sta3_ip])
 
 loop = asyncio.new_event_loop()
 #Make sure other modules which support concurrency use the same loop
@@ -149,20 +129,6 @@ if sta3_ip:
     stas_list.append(sta3)
 
 ssh_node.open_consoles(silent_mode=True)
-
-netlinks = []
-if args.netlink :
-     if sta1_ip and yaml['TRAFFIC_DST1']['brcm_chip'].startswith('4389') :
-         netlinks.extend([netlink_pktts(sshnode=sta1, debug=True, silent=False, chip='4389')])
-     if sta2_ip and yaml['TRAFFIC_DST2']['brcm_chip'].startswith('4389') :
-         netlinks.extend([netlink_pktts(sshnode=sta2, debug=True, silent=False, chip='4389')])
-     if sta3_ip and yaml['TRAFFIC_DST3']['brcm_chip'].startswith('4389') :
-         netlinks.extend([netlink_pktts(sshnode=sta3, debug=True, silent=False, chip='4389')])
-     if netlinks :
-         netlink_pktts.commence()
-
-if args.apdump:
-    ap = ssh_node(name='ap', ipaddr=yaml['AP']['control_ip'], sshtype='ush', relay=yaml['AP']['port_forward'])
 
 # LSA - latency sensitive with tos VI
 # LIA - latency insensitive with tos BE
@@ -236,43 +202,19 @@ if args.ampdudump:
 try:
     if traffic_flows:
         for runid in range(args.runcount) :
-
             run_log_file = os.path.join(args.output_directory, 'main_run{}.log'.format(str(runid + 1)))
             logging.basicConfig(filename=run_log_file, force=1, filemode='w', level=numeric_level, format='%(asctime)s %(name)s %(module)s %(levelname)-8s %(message)s')
-
-            if args.apdump:
-                logging.info("Ap Periodic Dump starts with Period={}".format(str(args.apdump_period)))
-                p = ap.periodic_cmd_enable(cmd='/bin/hw_accel_dump.sh', time_period=args.apdump_period, cmd_log_file=os.path.join(args.output_directory, 'ap_dump_run{}.log'.format(str(runid + 1))))
-
             for traffic_flow in traffic_flows:
                 tmp = "Running ({}/{}) {} traffic client={} server={} dest={} with load {} for {} seconds".format(str(runid+1), str(args.runcount), traffic_flow.proto, traffic_flow.client, traffic_flow.server, traffic_flow.dstip, traffic_flow.offered_load, args.time)
                 logging.info(tmp)
                 print(tmp)
             gc.disable()
             iperf_flow.run(time=args.time, flows='all')
-            for netlink in netlinks :
-                netlink.await_io_finish()
             gc.enable()
-            for netlink in netlinks :
-                logging.info('Netlink: {} stdout={} stderr={}'.format(netlink.name, netlink.stdout_linecount, netlink.stderr_linecount))
-                print('Netlink: {} stdout={} stderr={}'.format(netlink.name, netlink.stdout_linecount, netlink.stderr_linecount))
-                if (netlink.stdout_linecount > 10) :
-                    directory = os.path.join(args.output_directory, 'netlink_histograms', 'run{}'.format(runid))
-                    logging.info('Netlink create {} histograms in directory {}'.format(netlink.name,  directory))
-                    try :
-                        logging.info('Flow id to name table = {}'.format(iperf_flow.flowid2name))
-                        netlink_pktts.CreateHistograms(directory=directory, run_number=runid, starttime=trfc1.starttime, endtime=trfc1.endtime, testtitle='LSA', flowtable=iperf_flow.flowid2name, population_min = 10)
-                    except:
-                        raise
-            netlink_pktts.ResetStats()
             try :
                 gc.collect()
             except:
                 pass
-
-            if args.apdump:
-                ssh_node.sleep(1, text="cleanup delay", stoptext="cleanup delay")
-                p = ap.periodic_cmds_stop()
 
         end_log_file = os.path.join(args.output_directory, 'main_end.log')
         logging.basicConfig(filename=end_log_file, force=1, filemode='w', level=numeric_level, format='%(asctime)s %(name)s %(module)s %(levelname)-8s %(message)s')
@@ -282,24 +224,9 @@ try:
     else:
         print("No traffic Flows instantiated per test {}".format(args.test_name))
 
-    if args.ampdudump:
-        for sta in stas_list :
-            cmd = "wl dump ampdu"
-            logging.info('ampdu dump for STA {} using ({})'.format(str(sta.ipaddr), cmd))
-            ret = sta.rexec(cmd=cmd, run_now=True)
-            cmd_results = ret.results.decode()
-            cmd_results = cmd_results.splitlines()
-
-            for line in cmd_results:
-                logging.info('{}'.format(str(line)))
-
 finally :
-    if args.netlink :
-        netlink_pktts.cease()
 
     ssh_node.close_consoles()
-
     if traffic_flows:
         iperf_flow.close_loop()
-
     logging.shutdown()
