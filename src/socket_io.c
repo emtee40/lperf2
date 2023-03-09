8887a003c33174feea7ee183d75eaed4886bb3e5
Robert McMahon <rjmcmahon@rjmcmahon.com>
2023-03-09 09:50:21 -0800

    print errno on writen fail

--- src/socket_io.c
+++ src/socket_io.c
@@ -211,6 +211,7 @@ int writen (int inSock, const void *inBuf, int inLen, int *count) {
 	case SOCKET_ERROR :
 	    if (!((errno == EINTR) || (errno == EAGAIN) || (errno == EWOULDBLOCK))) {
 		nwritten = inLen - nleft;
+		printf("**** writen errno = %d\n", errno);
 		WARN_errno(1, "writen fatal");
 		sInterupted = 1;
 		goto DONE;
