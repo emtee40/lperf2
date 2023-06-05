509f98dbedf0fdba1014df26d195cac083d8561e
Robert McMahon <rjmcmahon@rjmcmahon.com>
2023-06-05 11:44:10 -0700

    more more multicast refactor

--- src/Client.cpp
+++ src/Client.cpp
@@ -152,29 +152,27 @@ void Client::mySockInit (void) {
     SockAddr_localAddr(mSettings);
     SockAddr_remoteAddr(mSettings);
 
-    int rc = -1;
+    // Multicast can bind to a send device & ip addr in two ways,
+    // 1) Use SO_BINDTODEVICE and a bind call
+    // 2) Use the socket option of IP_MULTICAST_IF or IPV6_MULTICAST_IF
+    // Number one typically requires root and takes priority
+    // Check to see if a device is bound to determine which tecnique to use
     char ifname[IFNAMSIZ];
     ifname[0] = '\0';
     bool try_bind = ((mSettings->mLocalhost != NULL) ? true : false);
 #if HAVE_DECL_SO_BINDTODEVICE
     socklen_t optlen = IFNAMSIZ;
-    rc = getsockopt(mSettings->mSock, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifname, &optlen);
+    int rc = getsockopt(mSettings->mSock, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifname, &optlen);
+    WARN_errno((rc != 0) , "getsockopt so_bindtodevice");
 #endif
-    if (isMulticast(mSettings) && isIPV6(mSettings) && mSettings->mIfrnametx && ((rc != 0) || (strlen(ifname) == 0))) {
+    if (isMulticast(mSettings) && isIPV6(mSettings) && mSettings->mIfrnametx && (strlen(ifname) == 0)) {
 	iperf_multicast_sendif_v6(mSettings);
     }
 
     if (try_bind) {
-	if (isMulticast(mSettings)) {
-	    // Multicast can bind to a send device & ip addr in two ways,
-	    // 1) Use SO_BINDTODEVICE and a bind call
-	    // 2) Use the socket option of IP_MULTICAST_IF or IPV6_MULTICAST_IF
-	    // Number one typically requires root and takes priority
-	    // Check to see if a device is bound to determine which tecnique to use
-	    if (!isIPV6(mSettings) && ((rc != 0) || (strlen(ifname) == 0))) {
-		if (iperf_multicast_sendif_v4(mSettings) == IPERF_MULTICAST_SENDIF_SUCCESS) {
-		    try_bind = false;
-		}
+	if (isMulticast(mSettings) && !isIPV6(mSettings) && (strlen(ifname) == 0)) {
+	    if (iperf_multicast_sendif_v4(mSettings) == IPERF_MULTICAST_SENDIF_SUCCESS) {
+		try_bind = false;
 	    }
 	}
 	if (try_bind) {
