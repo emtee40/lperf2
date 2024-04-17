ac800ca3797195aeba87efb7704600a5ac413fb9
Robert McMahon <rjmcmahon@rjmcmahon.com>
2024-04-17 09:30:35 -0700

    fixes to isoch latency suppressed output

--- include/Locale.h
+++ include/Locale.h
@@ -286,6 +286,8 @@ extern const char report_bw_jitter_loss_enhanced_isoch_triptime_format[];
 
 extern const char report_bw_jitter_loss_suppress_enhanced_format[];
 
+extern const char report_bw_jitter_loss_suppress_isoch_format[];
+
 extern const char report_sum_bw_jitter_loss_enhanced_format[];
 
 extern const char report_sumcnt_bw_jitter_loss_enhanced_format[];
