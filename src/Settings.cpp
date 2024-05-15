a817fb58741da8df34fecbd19d08edcbbed29f9c
Robert McMahon <rjmcmahon@rjmcmahon.com>
2024-05-14 17:19:59 -0700

    remove superfulous printf

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1727,7 +1727,6 @@ void Settings_ModalOptions (struct thread_Settings *mExtSettings) {
 		    struct timeval t;
 		    t.tv_sec = now.getSecs();
 		    t.tv_usec = now.getUsecs();
-		    printf("**** set prec %d %d %d\n", Microseconds, Milliseconds, Seconds);
 		    iperf_formattime(timestr, sizeof(timestr), t, (isEnhanced(mExtSettings) ? Milliseconds : Seconds), isUTC(mExtSettings), YearThruSecTZ);
 		    printf(error_starttime_exceeds, mExtSettings->txstart_epoch.tv_sec, mExtSettings->txstart_epoch.tv_usec, \
 			  timestr, MAXDIFFTXSTART);
